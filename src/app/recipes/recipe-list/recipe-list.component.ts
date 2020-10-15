7dce4907bbd3c30421906581960d30e11c37c85b
danowicz <contact@danowicz.dev>
2020-10-15 09:30:12 -0300

    ingredient list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,18 +3,17 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is simple a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636'),
-    new Recipe('Another test', 'Another simple test', 'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg')
-  ];
-  constructor() { }
+export class RecipeListComponent {
+	public recipes: Recipe[] = [
+		new Recipe('A test Recipe', 'This is simple a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636'),
+		new Recipe('Another test', 'Another simple test', 'https://joyfoodsunshine.com/wp-content/uploads/2016/09/easy-pizza-casserole-recipe-4-500x500.jpg')
+	];
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+
 
 }
